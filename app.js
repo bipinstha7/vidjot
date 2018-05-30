b0faba74208bea190af54adfb3ac5e71ad5387c7
bipinstha7 <bipin.shrestha901@gmail.com>
2018-05-30 17:49:26 +0545

    update route

--- app.js
+++ app.js
@@ -1,29 +1,35 @@
- const express = require("express");
+const express = require("express");
+const exphbs = require("express-handlebars");
+const mongoose = require("mongoose");
 const app = express();
 
-// middleware
-app.use((req, res, next) => {
-    console.log((new Date).getFullYear());
-    next();
-});
+// connect to mongoose
+mongoose.connect("mongodb://bipin:bipin1@ds241530.mlab.com:41530/vidjot")
+.then(() => console.log("mongodb/mlab connected"))
+.catch(err => console.log("Error on connecting mongodb/mlab", err));
+
+// handlebars middleware
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 // index route
 app.get("/", (req, res) => {
-    res.send("This is index/home route");
+	const title = "Welcome "
+	res.render("index", { title: title });
 });
 
 // about route
 app.get("/about", (req, res) => {
-    res.send("About Page");
+	res.render("about");
 });
 
 // handle unknown route
 app.get("/*", (req, res) => {
-    res.status(400).send("404 URL is not recognized");
+	res.status(400).send("404 URL is not recognized");
 });
 
 
 const port = process.env.port || 3000;
 app.listen(port, () => {
-    console.log(`server is running on port: ${port}`);
+	console.log(`server is running on port: ${port}`);
 });
