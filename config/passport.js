94c0e03588874dfb281893fa9688a0f3bcf9de62
bipinstha7 <bipin.shrestha901@gmail.com>
2019-03-30 19:00:51 +0545

    passport configuration

--- config/passport.js
+++ config/passport.js
@@ -1,43 +1,48 @@
-const localStrategy = require("passport-local").Strategy;
-const bcrypt = require("bcryptjs");
+const localStrategy = require('passport-local').Strategy
+const bcrypt = require('bcryptjs')
 
 // load user model
-const User = require("../models/User");
+const User = require('../models/User')
 
 module.exports = function(passport) {
-  passport.use(new localStrategy(
-    // by default, local strategy uses username and password, we will override with email
-    { usernameField: "email", 
-      // allows us to pass back the entire request to the callback,
-      // here used to access flash message
-      passReqToCallback: true
-    }, (req, email, password, done) => {
-      User.findOne({email: email})
-        .then(user => {
-          if(!user) {
-            return done(null, false, req.flash("error_msg", "No user found"));
-          }
+	passport.use(
+		new localStrategy(
+			// by default, local strategy uses username and password, we will override with email
+			{
+				usernameField: 'email',
+				// allows us to pass back the entire request to the callback,
+				// here used to access flash message
+				passReqToCallback: true
+			},
+			(req, email, password, done) => {
+				User.findOne({ email: email })
+					.then(user => {
+						if (!user) {
+							return done(null, false, req.flash('error_msg', 'No user found'))
+						}
 
-          // match password
-          bcrypt.compare(password, user.password, (err, isMatch) => {
-            if(err) throw err;
-            if(isMatch) {
-              return done(null, user, req.flash("success_msg", "You are logged in"));
-            } else {
-              return done(null, false, req.flash("error_msg", "password incorrect"));
-            }
-          });
-        })
-        .catch(err => console.log("localStrategy: Something gone wrong", err));
-  }));
+						// match password
+						bcrypt.compare(password, user.password, (err, isMatch) => {
+							if (err) throw err
+							if (isMatch) {
+								return done(null, user, req.flash('success_msg', 'You are logged in'))
+							} else {
+								return done(null, false, req.flash('error_msg', 'password incorrect'))
+							}
+						})
+					})
+					.catch(err => console.log('localStrategy: Something gone wrong', err))
+			}
+		)
+	)
 
-  passport.serializeUser((user, done) => {
-    done(null, user.id);
-  });
-  
-  passport.deserializeUser((id, done) => {
-    User.findById(id, (err, user) => {
-      done(err, user);
-    });
-  });
+	passport.serializeUser((user, done) => {
+		done(null, user.id)
+	})
+
+	passport.deserializeUser((id, done) => {
+		User.findById(id, (err, user) => {
+			done(err, user)
+		})
+	})
 }
