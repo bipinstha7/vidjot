059133706f9a698805c08ecd08481c7949d48ea1
bipinstha7 <bipin.shrestha901@gmail.com>
2019-03-30 19:01:25 +0545

    helper auth to check the user authentication

--- helpers/auth.js
+++ helpers/auth.js
@@ -1,22 +1,20 @@
 // prevent unauthorized user to access data
 module.exports = {
-  ensureAuthenticated: function (req, res, next) {
-    if (req.isAuthenticated()) {
-      return next();
-    }
-    req.flash("error_msg", "Not Authorized: You need to be logged in first");
-    res.redirect("/users/login");
-  },
+	ensureAuthenticated: function(req, res, next) {
+		if (req.isAuthenticated()) {
+			return next()
+		}
+		req.flash('error_msg', 'Not Authorized: You need to be logged in first')
+		res.redirect('/users/login')
+	},
 
-  // prevent logged in users to enter in login and logout page
-  isLoggedinAlready: function (req, res, next) {
-    if (req.isAuthenticated()) {
-      req.flash("success_msg", "You are already logged in");
-      res.redirect("back");
-    } else {
-        next();
-    }
-  }
+	// prevent logged in users to enter in login and logout page
+	isLoggedinAlready: function(req, res, next) {
+		if (req.isAuthenticated()) {
+			req.flash('success_msg', 'You are already logged in')
+			res.redirect('back')
+		} else {
+			next()
+		}
+	}
 }
-
-
